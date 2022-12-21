a31166dad15a7661602177dbf962e7980939f3fb
Nicola-besana <115076634+Nicola-besana@users.noreply.github.com>
2022-12-21 20:58:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+float f(float y) {
+
+    float x=0;
+    x = pow(y,2)*cos(y)+1;
+    return x;
+}
+
 int main() {
-   return 0;
+    float a = 0, b = 0, x = 0, err = 1;
+    do {
+        cout << "Inserire gli estremi:";
+        cin >> a;
+        cin >> b;
+    } while (f(a) * f(b) >= 0);
+
+
+while(f(x) !=0) {
+    x = (a + b) / 2;
+    if (f(a) * f(b) < 0) {
+            b = x;
+        } else {
+            a = x;
+        }
+        err = ((b - a) / 2);
+    if(err >= (1e-6)){
+        break;
+    }
+}
+
+
+    cout << int(x*10000)/10000.0<<endl;
+    return 0;
 }
